ff5f0f238cd43b29640013644c0e260037739c2f
ShriShail <shrishail.halloor@quantumberg.com>
2024-09-30 11:27:41 +0530

    commit

--- HRMS Application/obj/Debug/net6.0/HRMS Application.AssemblyInfo.cs
+++ HRMS Application/obj/Debug/net6.0/HRMS Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f770939a1982c3809759107eca64a069738b2a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2026d7dff3b667108886f593782c3bc3124dfc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
