577df22d8c3361f441bc8f7ecbdda0a5d8491617
Guruprasd <wequantumberg@gmail.com>
2024-10-02 13:38:42 +0530

    gitignore

--- HRMS Unit Test/obj/Debug/net6.0/HRMS Unit Test.AssemblyInfo.cs
+++ HRMS Unit Test/obj/Debug/net6.0/HRMS Unit Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS Unit Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb78765dfe36a86990f50ffe032b443dc458a639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS Unit Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS Unit Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
